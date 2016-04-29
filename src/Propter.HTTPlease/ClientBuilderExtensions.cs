8d21b9b9e92fb5b052b53b8d02406ab6cc6b59b5
tintoy <tintoy@tintoy.io>
2016-04-29 16:55:38 +1000

    Implement HTTPlease integration.

---
+++ src/Propter.HTTPlease/ClientBuilderExtensions.cs
@@ -0,0 +1,32 @@
+﻿using HTTPlease;
+using System;
+
+namespace Propter
+{
+	using MessageHandlers;
+
+	/// <summary>
+	///		Propter Hoc extension methods for the <see cref="ClientBuilder">HTTP client builder</see>.
+	/// </summary>
+    public static class ClientBuilderExtensions
+    {
+		/// <summary>
+		///		Create a copy of the <see cref="ClientBuilder"/>, enabling activity-correlation support for its clients.
+		/// </summary>
+		/// <param name="clientBuilder">
+		///		The HTTP client builder.
+		/// </param>
+		/// <returns>
+		///		The new HTTP client builder.
+		/// </returns>
+		public static ClientBuilder WithActivityCorrelation(this ClientBuilder clientBuilder)
+		{
+			if (clientBuilder == null)
+				throw new ArgumentNullException(nameof(clientBuilder));
+
+			return clientBuilder.AddHandler(
+				() => new CorrelationMessageHandler()
+			);
+		}
+    }
+}
---
+++ src/Propter.HTTPlease/MessageHandlers/CorrelationMessageHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Propter.MessageHandlers
+{
+	/// <summary>
+	///		HTTP message handler that adds the "X-ActivityId" header to outgoing request messages using the ambient <see cref="ActivityCorrelationManager.ActivityId"/>.
+	/// </summary>
+	public class CorrelationMessageHandler
+		: DelegatingHandler
+    {
+		/// <summary>
+		///		Create a new <see cref="CorrelationMessageHandler"/>.
+		/// </summary>
+		public CorrelationMessageHandler()
+		{
+		}
+
+		/// <summary>
+		///		Asynchronously process an HTTP request message and its response.
+		/// </summary>
+		/// <param name="request">
+		///		The outgoing <see cref="HttpRequestMessage"/>.
+		/// </param>
+		/// <param name="cancellationToken">
+		///		A <see cref="CancellationToken"/> that can be used to cancel the asynchronous operation.
+		/// </param>
+		/// <returns>
+		///		The incoming HTTP response message.
+		/// </returns>
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			if (request == null)
+				throw new ArgumentNullException(nameof(request));
+
+			Guid? activityId = ActivityCorrelationManager.GetCurrentActivityId();
+			if (activityId.HasValue)
+			{
+				request.Headers.Remove("X-ActivityId");
+				request.Headers.Add("X-ActivityId",
+					activityId.Value.ToString()
+				);
+			}
+
+			return base.SendAsync(request, cancellationToken);
+		}
+    }
+}
